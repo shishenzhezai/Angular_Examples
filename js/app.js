990990526bf2aca61d440eef1107e4e51c84ae8a
shishenzhezai <1316147236@qq.com>
2017-02-07 16:17:30 +0800

    first step The first step to add

--- js/app.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-// import template from './src/html/index.html';
-
-import controller from './jisuanqi.js'
-
-moudel.exports= angular.moudel('myApp',[]).controller("myController",controller);
--- js/index.js
+++
@@ -1,6 +0,0 @@
-var angular =require('angular')
-// import template from './src/html/index.html';
-
-import controller from './jisuanqi.js'
-
- angular.module('myApp',[]).controller('myController',controller);
